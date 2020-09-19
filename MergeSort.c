cd1c770f4a3b8f2458c0a3d7a8b4cf49a13c8c48
Acasune <tttt4aaaa@gmail.com>
2020-09-19 19:17:42 +0900

    some sort algorithm

---
+++ MergeSort.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void Merge(int A[], int tmp[], int, int l, int r);
+void MergeSort(int A[], int tmp[], int l, int r);
+
+void MergeSort(int A[], int tmp[], int l, int r)
+{
+  int mid;
+  if (r > l)
+  {
+    mid = (l + r) / 2;
+    MergeSort(A, tmp, l, mid);
+    MergeSort(A, tmp, mid + 1, r);
+    Merge(A, tmp, l, mid + 1, r);
+  }
+}
---
+++ QuickSort.c
@@ -0,0 +1,62 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int Partition(int *A, int low, int high);
+void QuickSort(int *A, int low, int high);
+
+void QuickSort(int *A, int low, int high)
+{
+  int pivot;
+  if (high > low)
+  {
+    pivot = Partition(A, low, high);
+    QuickSort(A, low, pivot - 1);
+    QuickSort(A, pivot, high);
+  }
+}
+
+int Partition(int *A, int low, int high)
+{
+  int left, right, pivot_item = A[low];
+  left = low;
+  right = high;
+  while (left < right)
+  {
+    while (A[left] <= pivot_item)
+      left++;
+    while (A[right] > pivot_item)
+      right--;
+    if (left < right)
+    {
+      int tmp = A[left];
+      A[left] = A[right];
+      A[right] = tmp;
+    }
+  }
+  A[low] = A[right];
+  A[right] = pivot_item;
+  return right;
+}
+
+void displayElemOfArray(int *A, int arraySize)
+{
+  for (int i = 0; i < 20; i++)
+  {
+    printf("%d ", A[i]);
+  }
+  printf("\n");
+}
+
+int main()
+{
+  int arraySize = 20;
+  int B[20];
+  for (int i = 0; i < arraySize; i++)
+  {
+    B[i] = rand() % 10 + 1;
+  }
+  printf("-----Initial State-----\n");
+  displayElemOfArray(B, arraySize);
+  QuickSort(B, 0, 20);
+  displayElemOfArray(B, arraySize);
+}
